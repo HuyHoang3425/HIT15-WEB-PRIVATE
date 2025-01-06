9043382405fda1a16e6d7470cb12270a79b07c39
bui huy hoang <buihoang34252@gmail.com>
2025-01-07 00:12:10 +0700

    bai3,4

---
+++ html-css-hit-15/week-11/bai-3/main.js
@@ -0,0 +1,34 @@
+let width = document.querySelector(".width");
+let height = document.querySelector(".height");
+let padding = document.querySelector(".padding");
+let margin = document.querySelector(".margin");
+let background = document.querySelector(".background");
+let img = document.querySelector(".img");
+let color = document.querySelector(".color");
+let content = document.querySelector(".content");
+let box = document.querySelector(".box");
+let draw = document.querySelector(".draw");
+let reset = document.querySelector(".reset")
+
+
+draw.addEventListener("click", picture);
+function picture() {
+    console.log(width.value);
+    // box.classList.remove("box");
+    
+    box.style.width = `${width.value}`;
+    box.style.height = `${height.value}`;
+    box.style.padding = `${padding.value}`;
+    box.style.margin = `${margin.value}`;
+    box.style.backgroundImage = `url("${img.value}")`;
+    box.style.backgroundRepeat = "no-repeat";
+    box.style.backgroundPosition = "center";
+    box.style.backgroundColor = `${background.value}`;
+    box.style.color = `${color.value}`;
+    box.innerHTML = content.value;
+}
+
+reset.addEventListener("click",()=>{
+    location.reload();
+});
+
---
+++ html-css-hit-15/week-11/bai-4/main.js
@@ -0,0 +1,57 @@
+let input = document.querySelector(".input");
+let table = document.querySelector(".table");
+let btn = document.querySelector(".card-btn");
+let exactly = document.querySelector("#exactly");
+let wrong = document.querySelector("#wrong");
+let result = document.querySelector(".result");
+let a  = "";
+btn.addEventListener("click",add);
+function add()
+{
+    table.innerHTML = "";
+    for(let i = 0;i < input.value.length;i++)
+    {
+        let div = document.createElement("div");
+        div.innerHTML = input.value[i];
+        div.classList.add("box");
+        table.appendChild(div);
+    }
+    
+    let i = -1;
+    let e = 0;
+    let w = 0;
+    document.addEventListener("keydown",(event) => {
+        console.log(event.key);
+        let box = document.querySelectorAll(".box");
+        i++;
+        // if (event.key === " ") {
+        //     event.preventDefault();
+        // }
+        event.preventDefault();
+            if(event.key == input.value[i])
+            {
+                box[i].classList.add("exactly_down");
+                e++;
+            }
+            else{
+                box[i].classList.add("wrong_down");
+                w++;
+            }
+            exactly.innerHTML = e;
+            wrong.innerHTML = w;
+            if (i + 1 === input.value.length) {
+                notification(e, w);
+            }
+    });
+    
+}
+function notification(e, w) {
+    if (e === input.value.length && w === 0) {
+        result.innerHTML = "đúng rồi bạn ơi!";
+    } else {
+        result.innerHTML = "Bạn nhập sai rồi!";
+    }
+}
+
+
+
